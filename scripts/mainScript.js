3775b86c4d0a46605da5989f45ee339c3b9a04d2
Jacky <lixufeng276@gmail.com>
2020-08-26 18:38:38 -0400

    added another data set to the chart

--- scripts/mainScript.js
+++ scripts/mainScript.js
@@ -3,6 +3,7 @@ var date = ["08/05", "08/06", "08/07", "08/08", "08/09", "08/10", "08/11"];
 var day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 //for the y-axis
 var apiCalls = [0, 15, 12, 12, 19, 8, 0]
+var apiCallsPrev = [0, 12, 14, 15, 3, 9, 0]
 
 var ctx = document.getElementById("apiCallsChart");
 var apiCallsChart = new Chart(ctx, {
@@ -11,7 +12,15 @@ var apiCallsChart = new Chart(ctx, {
         labels: date,
         datasets: [
             {
-                data: apiCalls
+                data: apiCalls,
+                label: "Total API calls",
+                borderColor: "black",
+                fill: false
+            },
+            {
+                data: apiCallsPrev,
+                label: "Total API calls previous week",
+                fill: false
             }
         ]
     }
